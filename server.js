1383a127ba49e01d7ba5e35c45ba91e52943bb48
Alisha Batham <alishabatham2@gmail.com>
2026-09-15 15:41:42 +0530

    Add fileURLToPath and dirname for module path

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ import dotenv from 'dotenv';
 import dns from 'node:dns';
 import nodemailer from 'nodemailer';
 import path from 'path';
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // DNS resolution fix for MongoDB Atlas SRV records
 dns.setDefaultResultOrder('ipv4first');
 try {
